efb0d9f86614c870261c65ce6dd9e5b56671e758
yiminghe <yiminghe@gmail.com>
2014-12-22 12:27:15 +0800

    fix gitignore

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,7 @@
 var yeoman = require('yeoman-generator');
 var Base = yeoman.generators.Base;
 var Path = require('path');
+var fs = require('fs');
 module.exports = Base.extend({
   constructor: function () {
     Base.apply(this, arguments);
@@ -11,6 +12,7 @@ module.exports = Base.extend({
 
   welcome: function () {
     this.appname = this.appname.replace(/\s/g, '-');
+    this.Appname = this.appname.charAt(0).toUpperCase() + this.appname.slice(1);
     this.log('welcome to generator-rc: ' + this.appname);
     this.port = this.options.port;
     this.version = this.options.version;
@@ -20,12 +22,27 @@ module.exports = Base.extend({
   setup: function () {
     var self = this;
     this.src.recurse('.', function (path, root, subdir, filename) {
-      console.log(arguments);
+      var oldPath = path;
+      // https://github.com/npm/npm/issues/1862
+      if (filename === 'gitignore') {
+        filename = '.gitignore';
+        var newPath = Path.join(Path.dirname(path), filename);
+        fs.renameSync(path, newPath);
+        path = newPath;
+      }
       self.template(path, subdir ? Path.resolve(subdir, filename) : filename);
+      if (filename === '.gitignore') {
+        fs.renameSync(path, oldPath);
+      }
     });
     var appname = this.appname;
-    var filename = appname.charAt(0).toUpperCase()+appname.slice(1);
-    this.dest.write('lib/' + filename + '.js', ['/**', '*' + filename, '*/'].join('\n'));
+    var filename = appname.charAt(0).toUpperCase() + appname.slice(1);
+    this.dest.write('lib/' + filename + '.js', [
+      '/**', '*' + filename, '*/',
+      'var React = require(\'react\');',
+      'var ' + filename + ' = React.createClass();',
+      'module.exports = ' + filename + ';'
+    ].join('\n'));
   },
 
   done: function () {
