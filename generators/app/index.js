394525a6bbc50fa9204316ba1443f90e14a363ab
yiminghe <yiminghe@gmail.com>
2015-06-23 19:42:41 +0800

    fix npmignore

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,8 +3,8 @@ var Base = yeoman.generators.Base;
 var Path = require('path');
 var fs = require('fs');
 
-function camelCase(name){
-  return name.replace(/-\w/g,function(m){
+function camelCase(name) {
+  return name.replace(/-\w/g, function (m) {
     return m.charAt(1).toUpperCase();
   })
 }
@@ -30,15 +30,16 @@ module.exports = Base.extend({
     var self = this;
     this.src.recurse('.', function (path, root, subdir, filename) {
       var oldPath = path;
+      var newPath;
       // https://github.com/npm/npm/issues/1862
-      if (filename === 'gitignore') {
-        filename = '.gitignore';
-        var newPath = Path.join(Path.dirname(path), filename);
+      if (filename === 'gitignore' || filename === 'npmignore') {
+        filename = filename === 'gitignore' ? '.gitignore' : '.npmignore';
+        newPath = Path.join(Path.dirname(path), filename);
         fs.renameSync(path, newPath);
         path = newPath;
       }
       self.template(path, subdir ? Path.resolve(subdir, filename) : filename);
-      if (filename === '.gitignore') {
+      if (filename === '.gitignore' || filename === '.npmignore') {
         fs.renameSync(path, oldPath);
       }
     });
